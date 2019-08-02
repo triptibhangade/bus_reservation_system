6da2520232cbc6298248b571cd3a586c95f3f92f
Tripti <tripti.bhangade@gemsessence.com>
2019-08-02 18:47:56 +0530

    experiment with searcg

--- app/controllers/buses_controller.rb
+++ app/controllers/buses_controller.rb
@@ -61,7 +61,7 @@ class BusesController < ApplicationController
       format.json { head :no_content }
     end
   end
-
+  
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_bus
