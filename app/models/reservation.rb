dc3d6ebd1f6d567fb60bc02c99d595b5ed6e8578
Tripti <tripti.bhangade@gemsessence.com>
2019-08-05 12:23:00 +0530

    Reservations Show Page Correction

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,7 @@
 class Reservation < ApplicationRecord
   # -------------------- Associations --------------------
   belongs_to :bus
+  belongs_to :users
   # -------------------- validations --------------------
   validates :reservation_date, presence:true
   validates :seat, presence:true
