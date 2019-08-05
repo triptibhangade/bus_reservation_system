bdae8305a2489fed5e0a9a14759949236f705df0
Tripti <tripti.bhangade@gemsessence.com>
2019-08-05 16:02:30 +0530

    Sign In/up page view setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,20 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   # --------------------------- Required Sign In -----------------------
-  def required_signin
-    unless current_user
-      flash[:error] = "Please Sign In Properly"
-      redirect_to root_path
-    end
-  end
+  # def required_signin
+  #   unless current_user
+  #     flash[:error] = "Please Sign In Properly"
+  #     redirect_to root_path
+  #   end
+  # end
   
   # ------------------------------ Required Sign Out ------------------------
-  def required_signout
-    if current_user
-      flash[:error] = "Please Sign out Properly"
-      redirect_to root_path
-    end
-  end
+  # def required_signout
+  #   if current_user
+  #     flash[:error] = "Please Sign out Properly"
+  #     redirect_to root_path
+  #   end
+  # end
 
   protected
   # -------------------- Device Params --------------------
