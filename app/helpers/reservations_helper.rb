95e15b3da73b90bf16a775d279c8995f9b8e0905
Tripti <tripti.bhangade@gemsessence.com>
2019-08-05 15:07:59 +0530

    view set resrvation

--- app/helpers/reservations_helper.rb
+++ app/helpers/reservations_helper.rb
@@ -15,7 +15,16 @@ module ReservationsHelper
     reservation.created_at.to_date.strftime("%d-%m-%Y")
   end
 
+  def day_of_booking(reservation)
+  # strftime("%m/%d/%Y")
+    reservation.created_at.to_date.strftime("%A")
+  end
+
   def reservation_date(reservation)
     reservation.reservation_date.strftime("%d-%m-%Y")
+  end
+
+  def reservation_day(reservation)
+    reservation.reservation_date.strftime("%A")
   end
 end
