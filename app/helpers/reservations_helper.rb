406706dfc54822bbdd7ca573dbcda51ad1a6097b
Tripti <tripti.bhangade@gemsessence.com>
2019-08-05 14:48:27 +0530

    Customer name in reservation index'

--- app/helpers/reservations_helper.rb
+++ app/helpers/reservations_helper.rb
@@ -1,2 +1,12 @@
 module ReservationsHelper
+  # -------------------- Name of customer --------------------
+  def customer_name(reservation)
+    if reservation.user_id.present?
+      customer = User.find(reservation.user_id)
+      customer.name
+    else
+      bus_owner = BusOwner.find(reservation.bus_owner_id)
+      bus_owner.name
+    end
+  end
 end
