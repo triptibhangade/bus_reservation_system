cddb0e568129a08e5f994937fa5c4f355218af80
Tripti <tripti.bhangade@gemsessence.com>
2019-08-24 15:37:52 +0530

    dectivate user account by admin

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -8,4 +8,13 @@ class Admin::UsersController < ApplicationController
     @user = current_user
   end
 
+   def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to admin_customers_path, notice: "Customer #{@user.name}'s account was successfully removed." }
+      format.json { head :no_content }
+    end
+  end
+
 end
