9a66db835915f1f2e18c4c0f635a95c7236b6418
Tripti <tripti.bhangade@gemsessence.com>
2019-08-28 11:46:36 +0530

    add back buttons in all list

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   # --------------------------- Required Admin Sign In -----------------------
   def required_admin_signin
-    unless current_user.try(:role)=="admin" && current_bus_owner
+    unless current_user.try(:role)=="admin" || current_bus_owner
       flash[:error] = "You are not admin"
       redirect_to root_path
     end
