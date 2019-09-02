0971aae68e8a49bc956d755b566b93295b4c60bb
Tripti <tripti.bhangade@gemsessence.com>
2019-09-02 12:54:19 +0530

    add comments in bus model

--- app/models/bus.rb
+++ app/models/bus.rb
@@ -1,19 +1,20 @@
 class Bus < ApplicationRecord
-  # -------------------- Association --------------------
+  # ----------------------------------------- Association -----------------------------------------
   belongs_to :bus_owner
   has_many :reservations, dependent: :destroy
   has_many :seats,
             through: :reservations, dependent: :destroy
 
-  # -------------------- Status of Bus Owner --------------------
+  # ------------------------------------- Status of Bus Owner -------------------------------------
   enum status: {active: "active", suspend: "suspend" }
 
-  # -------------------- Validations --------------------
+  # ----------------------------------------- Validations -----------------------------------------
   validates :name, :registration_no, :source, :destination, presence:true
   validates :source, presence: true, format: { with: /\A[a-z A-Z]+\z/,
               message: "only allows capital letters" }
   validate :total_no_of_seats_is_divided_by_four
-  # -------------------- Search --------------------
+  
+  # -------------------------------------------- Search --------------------------------------------
   scope :filter_by_source, -> (source) { where(source: source) }
   scope :filter_by_destination, -> (destination) { where(destination: destination) }
   scope :filter_by_source_and_destination, -> (source, destination) { filter_by_source(source).filter_by_destination(destination) }
